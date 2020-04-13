f36b51038ec67b6a7bf1e3a3d784b13c0126a959
Todd Costella <ToddCostella@gmail.com>
2020-04-13 16:31:39 -0600

    Added explicit support for testing indexes and merges.

--- src/main/groovy/com/menome/messageProcessor/MessageProcessor.groovy
+++ src/main/groovy/com/menome/messageProcessor/MessageProcessor.groovy
@@ -1,6 +1,7 @@
 package com.menome.messageProcessor
 
 import groovy.json.JsonSlurper
+import org.apache.commons.lang3.math.NumberUtils
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -8,22 +9,36 @@ class MessageProcessor {
 
     static Logger log = LoggerFactory.getLogger(MessageProcessor.class)
 
-    def statements = []
+    static statements = []
 
-    def process(String msg) {
+    static process(String msg) {
         if (msg) {
-            def parser = new JsonSlurper()
-            def msgMap = parser.parseText(msg) as Map
-            statements.addAll(processIndexes(msgMap))
-            statements.addAll(processMerges(msgMap))
+            statements.addAll(processIndexes(msg))
+            statements.addAll(processMerges(msg))
         }
     }
 
-    List<String> getNeo4JStatements() {
+    static List<String> processMerges(String msg) {
+        if (msg) {
+            def parser = new JsonSlurper()
+            def msgMap = parser.parseText(msg) as Map
+            return processMerges(msgMap)
+        }
+    }
+
+    static List<String> processIndexes(String msg) {
+        if (msg) {
+            def parser = new JsonSlurper()
+            def msgMap = parser.parseText(msg) as Map
+            return processIndexes(msgMap)
+        }
+    }
+
+    static List<String> getNeo4JStatements() {
         statements
     }
 
-    List<String> processIndexes(Map msgMap) {
+    static List<String> processIndexes(Map msgMap) {
         def indexStatements = []
         def conformedDimensions = (Map) msgMap.ConformedDimensions
         def keys = (conformedDimensions ?: [:]).keySet().join(",")
@@ -49,16 +64,22 @@ class MessageProcessor {
         }
     */
 
-    def List<String> processMerges(Map msgMap) {
+    def static List<String> processMerges(Map msgMap) {
         def mergeStatements = []
         // Card Merge MERGE (node:Card:Employee {Email: "konrad.aust@menome.com",EmployeeId: 12345})
-        def cardMerge = "MERGE (node:Card: $msgMap.NodeType "
+        def cardMerge = "MERGE (node:Card:$msgMap.NodeType "
 
-        Map conformedDimensions = msgMap.ConformedDimensions
+        def conformedDimensions = msgMap.ConformedDimensions
         if (conformedDimensions) {
             cardMerge += "{"
             conformedDimensions.eachWithIndex { key, value, index ->
-                cardMerge += "$key: $value" + (index < conformedDimensions.size() - 1 ? "," : "")
+                boolean valueIsNumeric = NumberUtils.isCreatable(value as String)
+                String valueDelimiter = ""
+                if (!valueIsNumeric) {
+                    valueDelimiter = "\""
+                }
+
+                cardMerge += "$key: $valueDelimiter$value$valueDelimiter" + (index < conformedDimensions.size() - 1 ? "," : "")
             }
             cardMerge += "})"
         }
--- src/test/groovy/com/menome/messageProcessor/MessageProcessorSpecification.groovy
+++ src/test/groovy/com/menome/messageProcessor/MessageProcessorSpecification.groovy
@@ -5,6 +5,15 @@ import spock.lang.Specification
 
 class MessageProcessorSpecification extends Specification {
 
+    static String simpleMessage = MessageBuilder.builder()
+            .Name("Konrad Aust")
+            .NodeType("Employee")
+            .Priority(1)
+            .SourceSystem("HRSystem")
+            .ConformedDimensions("Email": "konrad.aust@menome.com", "EmployeeId": 12345)
+            .build()
+            .toJSON()
+
     def "process invalid empty message"() {
         given:
         def msg = ""
@@ -14,29 +23,54 @@ class MessageProcessorSpecification extends Specification {
         processor.getNeo4JStatements().isEmpty()
     }
 
+    MessageProcessor processSimpleMessage() {
+        MessageProcessor processor = new MessageProcessor()
+        processor.process(simpleMessage)
+        return processor
+    }
+
+    String getIndexFromList(List<String> indices, String indexName) {
+        String index = ""
+        indices.each() {
+            if (it.contains(indexName)) {
+                index = it
+            }
+        }
+        return index
+    }
+
+
     def "process simple valid message"() {
         given:
-        String msg = MessageBuilder.builder()
-                .Name("Konrad Aust")
-                .NodeType("Employee")
-                .Priority(1)
-                .SourceSystem("HRSystem")
-                .ConformedDimensions("Email": "konrad.aust@menome.com", "EmployeeId": 12345)
-                .build()
-                .toJSON()
-        MessageProcessor processor = new MessageProcessor()
-        processor.process(msg)
+        def processor = processSimpleMessage()
         List<String> statements = processor.getNeo4JStatements()
 
         expect:
-        def expectedStatements = [
-                "CREATE INDEX ON :Card(Email,EmployeeId)"
-                ,"CREATE INDEX ON :Employee(Email,EmployeeId)"
-                ,"MERGE (node:Card:Employee {Email: \"konrad.aust@menome.com\",EmployeeId: 12345}) ON CREATE SET node.Uuid = {newUuid} SET node += {nodeParams} SET node.TheLinkAddedDate = datetime()"
-        ]
-        statements.size() == expectedStatements.size()
-        expectedStatements.each() { statement ->
-            statements.contains(statement)
-        }
+        statements.size() == 3
+    }
+
+    def "process indexes from simple message"() {
+        given:
+        def expectedCardIndex = "CREATE INDEX ON :Card(Email,EmployeeId)"
+        def expectedEmployeeIndex = "CREATE INDEX ON :Employee(Email,EmployeeId)"
+        def processor = processSimpleMessage()
+        List<String> indexStatements = processor.processIndexes(simpleMessage)
+
+        expect:
+        indexStatements.size() == 2
+        def actualCardIndex = getIndexFromList(indexStatements, ":Card")
+        def actualEmployeeIndex = getIndexFromList(indexStatements, ":Employee")
+        actualCardIndex == expectedCardIndex
+        actualEmployeeIndex == expectedEmployeeIndex
+
+    }
+
+    def "process merge from simple message"() {
+        given:
+        def processor = processSimpleMessage()
+        List<String> mergeStatements = processor.processMerges(simpleMessage)
+        expect:
+        mergeStatements.size() == 1
+        mergeStatements[0] == "MERGE (node:Card:Employee {Email: \"konrad.aust@menome.com\",EmployeeId: 12345}) ON CREATE SET node.Uuid = {newUuid} SET node += {nodeParams} SET node.TheLinkAddedDate = datetime()"
     }
 }
