a21ec1d941d88e9852c9cfce4f0c1cfc80d6b6f8
Todd Costella <ToddCostella@gmail.com>
2020-04-24 15:14:05 -0600

    5000 message test

--- src/test/groovy/com/menome/rabbitIntegration/RabbitMQVolumeSpecification.groovy
+++ src/test/groovy/com/menome/rabbitIntegration/RabbitMQVolumeSpecification.groovy
@@ -1,15 +1,22 @@
 package com.menome.rabbitIntegration
 
 import com.menome.MessagingSpecification
+import com.menome.messageProcessor.MessageProcessor
+import com.menome.util.Neo4J
 import com.rabbitmq.client.*
 import com.rabbitmq.client.impl.MicrometerMetricsCollector
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry
+import org.neo4j.driver.Driver
+import org.neo4j.driver.Session
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.Network
 import spock.lang.Shared
 
+import java.text.SimpleDateFormat
+import java.time.Duration
+import java.time.Instant
 import java.util.concurrent.TimeUnit
 
 import static org.awaitility.Awaitility.await
@@ -27,11 +34,27 @@ class RabbitMQVolumeSpecification extends MessagingSpecification {
     @Shared
     static MicrometerMetricsCollector metrics
 
+    @Shared
+    GenericContainer neo4JContainer
+
+    @Shared
+    Driver neo4JDriver
+
 
     protected static ConnectionFactory createRabbitConnectionFactory(GenericContainer rabbitMQContainer){
         ConnectionFactory rabbitConnectionFactory = new ConnectionFactory()
         rabbitConnectionFactory.host = rabbitMQContainer.containerIpAddress
         rabbitConnectionFactory.port = rabbitMQContainer.getMappedPort(RABBITMQ_PORT)
+        rabbitConnectionFactory.username = "menome"
+        rabbitConnectionFactory.password = "menome"
+
+        return rabbitConnectionFactory
+    }
+
+    protected static ConnectionFactory createRabbitConnectionFactory(){
+        ConnectionFactory rabbitConnectionFactory = new ConnectionFactory()
+        rabbitConnectionFactory.host = "127.0.0.1"
+        rabbitConnectionFactory.port = RABBITMQ_PORT
         rabbitConnectionFactory.username = "menome"
         rabbitConnectionFactory.password = "menome"
 
@@ -50,10 +73,12 @@ class RabbitMQVolumeSpecification extends MessagingSpecification {
     }
 
     def setupSpec(){
-        rabbitMQContainer = createAndStartRabbitMQContainer(Network.newNetwork())
-        rabbitConnectionFactory = createRabbitConnectionFactory(rabbitMQContainer)
+        //rabbitMQContainer = createAndStartRabbitMQContainer(Network.newNetwork())
+        rabbitConnectionFactory = createRabbitConnectionFactory()
         metrics = new MicrometerMetricsCollector(new SimpleMeterRegistry())
         rabbitConnectionFactory.setMetricsCollector(metrics)
+        neo4JContainer = createAndStartNeo4JContainer(Network.newNetwork())
+        neo4JDriver =  Neo4J.openDriver(neo4JContainer)
     }
 
     def "write 5000 messages to rabbit"() {
@@ -61,21 +86,34 @@ class RabbitMQVolumeSpecification extends MessagingSpecification {
         def rabbitChannel = openRabbitMQChanel(rabbitMQContainer, RABBITMQ_QUEUE_NAME, RABBITMQ_TEST_EXCHANGE, RABBITMQ_TEST_ROUTING_KEY)
         when:
         (1..5000).each { it->
-            String msg = "Some Message $it"
-            rabbitChannel.basicPublish(RABBITMQ_TEST_EXCHANGE, RABBITMQ_TEST_ROUTING_KEY, null, msg.getBytes())
+            String message = messageWithConnections.replaceAll("konrad.aust@menome.com","konrad.aust$it@menome.com")
+            rabbitChannel.basicPublish(RABBITMQ_TEST_EXCHANGE, RABBITMQ_TEST_ROUTING_KEY, null, message.getBytes())
         }
         await().atMost(5, TimeUnit.MINUTES).until { metrics.publishedMessages.count() == 5000 }
         then:
-        1 == 1
+        println(new SimpleDateFormat("yyyy/MM/dd HH:mm:SSSSS").format(new Date()) )
+        Instant start = Instant.now()
         rabbitChannel.basicConsume(RABBITMQ_QUEUE_NAME, new DefaultConsumer(rabbitChannel) {
             @Override
             void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-                long deliveryTag = envelope.getDeliveryTag();
-                String foo = new String(body)
-                println foo
-                rabbitChannel.basicAck(deliveryTag, false);
+                long deliveryTag = envelope.getDeliveryTag()
+                MessageProcessor processor = new MessageProcessor()
+                Session session = neo4JDriver.session()
+
+                processor.process(new String(body))
+                //println new String(body)
+                List<String> statements = processor.getNeo4JStatements()
+                Neo4J.executeStatementListInSession(statements, session)
+                session.close()
+
+                rabbitChannel.basicAck(deliveryTag, false)
             }
         })
-        await().atMost(5, TimeUnit.MINUTES).until { metrics.consumedMessages.count() == 5000 }
+        await().atMost(5, TimeUnit.MINUTES).until { metrics.consumedMessages.count() >= 5000 }
+        Instant end = Instant.now()
+        Duration duration = Duration.between(start,end)
+        println(duration)
+        println "Done..."
+        //sleep(100000)
     }
 }
