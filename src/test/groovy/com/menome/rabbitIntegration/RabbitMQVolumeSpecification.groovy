9fb05360f582474c7d4a83e2f363117b4dabc4b5
Todd Costella <tcostella@menome.com>
2020-06-17 16:38:03 -0600

    LNK-170 Forgot to @Ignore the rabbit test.

--- src/test/groovy/com/menome/rabbitIntegration/RabbitMQVolumeSpecification.groovy
+++ src/test/groovy/com/menome/rabbitIntegration/RabbitMQVolumeSpecification.groovy
@@ -36,6 +36,7 @@ class RabbitMQVolumeSpecification extends SymendMessagingSpecification {
         rabbitConnectionFactory.setMetricsCollector(metrics)
     }
 
+    @Ignore
     def "write 50,000 Employee Messages to rabbit"() {
         given:
         rabbitConnectionFactory = createRabbitConnectionFactory()
